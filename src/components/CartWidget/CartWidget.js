6837da4cd5461355063a8cb19a169b65cf7f0fc3
jonatan padilla <padilla.joni@gmail.com>
2024-02-22 23:19:34 -0300

    segunda entrega

---
+++ src/components/CartWidget/CartWidget.js
@@ -0,0 +1,13 @@
+// import carrito from './img/carrito.jpg'
+
+const CartWidget = () => {
+    return (
+        <div>
+            <h3>👤</h3>
+            
+            {/* <img src={carrito} alt="cartwidget"/> */}
+        </div>
+    )
+}
+
+export default CartWidget;
---
+++ src/components/NavBar/Navbar.js
@@ -0,0 +1,25 @@
+// import logo from './img/logoprincipal.png'
+import CartWidget from '../CartWidget/CartWidget';
+
+
+const NavBar = () => {
+    return (
+        <nav>
+            <div className="conteiner">
+                sistema de asistencia
+                {/* <img className="logo" src={logo} alt="src" /> */}
+                <div>
+                    <button>Remeras</button>
+                    <button>Short</button>
+                    <button>Medias</button>
+                    <button>Accesorios</button>
+                </div>
+                <div>
+                    <CartWidget />
+                </div>
+            </div>
+        </nav>
+    )
+}
+
+export default NavBar;
